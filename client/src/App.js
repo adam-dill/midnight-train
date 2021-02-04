10ca124debb52af425938d9bdc3d87a83cb1d2db
Adam Dill <adamtdill@gmail.com>
2021-02-04 12:03:38 -0600

    Adjusting timezone on the output.

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
     }
 
     processEntry(entry) {
-        const date = moment(entry.time)
+        const date = moment(entry.time).utcOffset(4);
         const day = date.format("ddd, MMMM Do");
         const time = date.format("h:mm a");
         return {
