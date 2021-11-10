9b9f0dcb87163aaebdb6609b641e8162e55c1a11
Adam Dill <adamtdill@gmail.com>
2021-11-09 18:17:46 -0600

    removing DST.

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ class App extends React.Component {
 
     processEntry(entry) {
         let date = moment(entry.time);
-        if (date.isDST() === false) { date.add(1, 'h') }
+        //if (date.isDST() === false) { date.add(1, 'h') }
         const day = date.format("ddd, MMMM Do");
         const time = date.format("h:mm a");
         return {
